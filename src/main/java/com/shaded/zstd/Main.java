f3b0a1a936d1a0d39785bced10b08644f9204e50
yangjf2019 <highfei2011@outlook.com>
2024-12-13 12:42:00 +0800

    Initial commit

---
+++ src/main/java/com/shaded/zstd/Main.java
@@ -0,0 +1,14 @@
+package com.shaded.zstd;
+
+/**
+ * @Author Created by Jeff Yang on 12/13/24 11:20.
+ * @Update date:
+ * @Project: Default (Template) Project
+ * @Package: com.shaded.zstd
+ * @Describe:
+ */
+public class Main {
+	public static void main (String[] args) {
+		System.out.println("Hello world!");
+	}
+}
